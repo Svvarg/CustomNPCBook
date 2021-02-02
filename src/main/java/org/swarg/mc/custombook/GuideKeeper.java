9cdc16709b3869abcd9d7cd9f15fec4a90cadb9e
Svvarg <stizerblack@gmail.com>
2021-02-02 12:57:14 +0300

    search guide keeper. hold keeperId dialogNbt - first book page

--- src/main/java/org/swarg/mc/custombook/GuideKeeper.java
+++ src/main/java/org/swarg/mc/custombook/GuideKeeper.java
@@ -4,12 +4,16 @@ import java.util.Iterator;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.util.ChatComponentStyle;
+import net.minecraft.util.ChatComponentTranslation;
 
 import noppes.npcs.Server;
 import noppes.npcs.NoppesUtilServer;
@@ -19,6 +23,8 @@ import noppes.npcs.controllers.DialogController;
 import noppes.npcs.controllers.DialogCategory;
 import noppes.npcs.controllers.DialogOption;
 import noppes.npcs.controllers.Dialog;
+import noppes.npcs.controllers.PlayerDialogData;
+import noppes.npcs.controllers.PlayerDataController;
 
 
 /**
@@ -30,9 +36,12 @@ public class GuideKeeper {
     private static final String GUIDE_ENTITY_NAME = "Guide";
 
     private static GuideKeeper instance;
-    /*For search entity only once*/
-    private static EntityNPCInterface keeper;
-    //private static NBTTagCompound nbtDialog;
+    /*EntityId of CustomNPC GuideKeeper which will be opened through the 
+      GuideBook. Keeper will be located in the persists chunks (Spawn)*/
+    private Integer keeperId;
+    /*Entry dialog writen to nbt - firs page*/
+    private NBTTagCompound dialogNbt;
+    private Integer dialogId;//dialogNbt.getInteger("DialogId");
 
 
     public static GuideKeeper instance() {
@@ -44,6 +53,7 @@ public class GuideKeeper {
 
 
     /**
+     * aka Open GuideBook GUI (CustomNPC Dialog via book)
      * ResearchTool /os o e cnn Guide \ dialogs \ -a index 0 \ -list
      * @param is
      * @param world
@@ -52,32 +62,26 @@ public class GuideKeeper {
     public void openGuideDialog(ItemStack is, World world, EntityPlayer player) {
         if (world != null && !world.isRemote && player != null) {
 
-            this.keeper = findGuideKeeper();
+            EntityNPCInterface keeper = checkOrFindGuideKeeper();//update keeperId on success
 
-            if (keeper == null || keeper.isDead) {
-                keeper = null;//import for reset removed from world npc-entity
-                /*DEBUG*/player.addChatMessage(new ChatComponentText("Not Found Guide Keeper"));
-            }
-            else if (keeper.dialogs == null || keeper.dialogs.size() == 0) {
-                //nbtDialog = null;//для того чтобы сбросить заготовку отправляемую клиенту
-                /*DEBUG*/player.addChatMessage(new ChatComponentText("No Found Dialog in Guide Keeper"));
-            }
-            else {
-                Iterator iter = keeper.dialogs.values().iterator();
+            if (isValidKeeperState(keeper)) {
 
-                // pull first dialog from keeper npc
-                while (iter.hasNext()) {
-                    DialogOption option = (DialogOption) iter.next();
-                    if (option != null && option.hasDialog()) {
-                        Dialog dialog = option.getDialog();
-                        if (dialog != null) {
-                            NoppesUtilServer.openDialog(player, keeper, dialog);
-                            return;
-                        }
-                    }
+                if (this.dialogNbt == null || this.dialogId == null) {
+                    //init dialogNBT by search first dialog in GuideKeeper
+                    setGuideDialog(keeper);
                 }
 
-                ///*DEBUG*/player.addChatMessage(new ChatComponentText("Error No Found Dialog in Guide Keeper 2"));
+                if (this.dialogNbt != null && this.dialogId != null && PlayerDataController.instance != null) {
+
+                    //OpenDialog in Player Client
+                    Server.sendData((EntityPlayerMP)player, EnumPacketClient.DIALOG, new Object[]{ keeperId, dialogNbt});
+
+                    // without this package, the dialog options don't open for some reason
+                    NoppesUtilServer.setEditingNpc(player, keeper);
+                }
+                else {
+                    ///*DEBUG*/System.out.println( "hasDialogNbt:" + (dialogNbt !=null) + " hasDialogId:" + (dialogId!=null) + " hasPlayerDataController:"+(PlayerDataController.instance!=null));
+                }
             }
         }
     }
@@ -85,32 +89,129 @@ public class GuideKeeper {
 
 
     /**
-     * Serach Guide Keeper CustomNPC by name
+     * If No KeeperId - Search In all loaded server worlds
+     * Else check by exists keeperId
+     * @param keeperId
      * @return
      */
-    private EntityNPCInterface findGuideKeeper() {
-        if (keeper == null) {
-            MinecraftServer srv = MinecraftServer.getServer();
-            if (srv != null && srv.worldServers != null) {
-                final WorldServer[] a = srv.worldServers;
-                for (int i = 0; a != null && i < a.length; i++) {
-                    WorldServer ws = a[i];
-                    if (ws != null && ws.loadedEntityList != null) {
-                        for (int j = 0; j < ws.loadedEntityList.size(); j++) {
-                            Entity e = (Entity)ws.loadedEntityList.get(j);
-                            if (GUIDE_ENTITY_NAME.equals(e.getCommandSenderName()) && e instanceof EntityNPCInterface) {
-                                return (EntityNPCInterface) e;
-                            }
+    private EntityNPCInterface checkOrFindGuideKeeper() {
+        this.keeperId = null;
+        MinecraftServer srv = MinecraftServer.getServer();
+        if (srv != null && srv.worldServers != null) {
+            final WorldServer[] aws = srv.worldServers;
+
+            //update exist keeperId and check for changes
+            if (keeperId != null && keeperId.intValue() > -1) {
+                for (int i = 0; i < aws.length; i++) {
+                    final WorldServer ws = aws[i];
+                    final Entity e = ws.getEntityByID(keeperId.intValue());
+                    if (isGuideKeeper(e)) {
+                        return (EntityNPCInterface) e;
+                    }
+                }
+            }
+
+            //Initial Search Keeper in all loaded worlds
+            //todo search only in persistens spawn area
+            for (int i = 0; i < aws.length; i++) {
+                WorldServer ws = aws[i];
+                if (ws != null && ws.loadedEntityList != null) {
+                    final Object[] es = ws.loadedEntityList.toArray();
+                    for (int j = 0; j < es.length; j++) {
+                        final Entity e = (Entity) es[j];
+                        if (isGuideKeeper(e)) {
+                            this.keeperId = e.getEntityId();//update
+                            return (EntityNPCInterface) e;
                         }
                     }
                 }
             }
-            return null;
         }
-        //already founded
+        return null;
+    }
+
+
+    public static boolean isGuideKeeper(Entity e) {
+        return e instanceof EntityNPCInterface && GUIDE_ENTITY_NAME.equals(e.getCommandSenderName());
+    }
+
+    /**
+     * Has GuideKeeper and exist Dialogs on keeper
+     * @param keeper
+     * @return
+     */
+    private boolean isValidKeeperState(EntityNPCInterface keeper) {
+        //reset GuideKeeper
+        if (keeper == null || keeper.isDead) {
+            this.keeperId  = null;//import for reset removed from world npc-entity
+            this.dialogNbt = null;
+            this.dialogId  = null;
+            sendGlobalMessage("DEBUG", "Not Found Guide Keeper");//player.addChatMessage(new ChatComponentText("?"));
+        }
+        //reset DialogNbt-workpiece
+        else if (keeper.dialogs == null || keeper.dialogs.size() == 0) {
+            this.dialogNbt = null;
+            this.dialogId = null;
+            sendGlobalMessage("DEBUG", "No Found Dialog in Guide Keeper");
+        }
+        //success
         else {
-            return keeper;
+            this.keeperId = keeper.getEntityId();//updated 2
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    /**
+     * pull first dialog from keeper npc and write once it to nbt
+     * update dialogNbt and dialogId
+     * @param keeper
+     */
+    private void setGuideDialog(EntityNPCInterface keeper) {
+        this.dialogNbt = null;
+        this.dialogId = null;
+
+        Iterator iter = keeper.dialogs.values().iterator();
+        while (iter.hasNext()) {
+            final DialogOption option = (DialogOption) iter.next();
+            if (option != null && option.hasDialog()) {
+                final Dialog dialog = option.getDialog();
+                if (dialog != null) {
+                    this.dialogId  = dialog.id;//dialogNbt.getInteger("DialogId");
+                    this.dialogNbt = dialog.writeToNBT(new NBTTagCompound());
+                    /*DEBUG*/sendGlobalMessage("DEBUG", "Found Dialog in Guide Keeper");
+                    return;
+                }
+            }
+        }
+    }
+
+    
+    /**
+     * Message to all players
+     * @param name
+     * @param message
+     */
+    public static void sendGlobalMessage(String name, String message) {
+        ServerConfigurationManager csm = MinecraftServer.getServer().getConfigurationManager();
+        if (csm != null) {
+            ChatComponentStyle msg = new ChatComponentTranslation(message); //new ChatComponentText(line);
+            csm.sendChatMsg(msg);
         }
     }
 
 }
+
+//NoppesUtilServer.openDialog(player, keeper, dialog);
+//final int dialogId = dialogNbt.getInteger("DialogId");
+//final Dialog pDialog = dialog.copy(player); it transmit available dialog options for specific player. not needed here
+
+////if not marked as read the player will not be able to activate dialogOptions
+//PlayerDialogData data = PlayerDataController.instance.getPlayerData(player).dialogData;
+//if (data != null && !data.dialogsRead.contains( dialogId )) {
+//    sendGlobalMessage("DEBUG", "Dialog marked as read");
+//    data.dialogsRead.add( dialogId );
+//}
